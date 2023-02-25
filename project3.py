4a6af560ce5215b662b0c0968e48d6c8a75277c9
montymi <mcmontanaro01@gmail.com>
2023-02-25 17:28:51 -0500

    Moving project3 solution to separate file

---
+++ project3.py
@@ -0,0 +1,23 @@
+import random
+
+def guessTheNumber():
+    # Generate a random number within a specified range
+    min_num = 1
+    max_num = 20
+    secret_num = random.randint(min_num, max_num)
+
+    # Prompt the user to guess the number
+    guess = None
+    while guess != secret_num:
+        guess = int(input(f"Guess a number between {min_num} and {max_num}: "))
+
+        # Check if the guess is correct
+        if guess == secret_num:
+            print("Congratulations! You guessed the number.")
+        elif guess < secret_num:
+            print("The number is higher.")
+        else:
+            print("The number is lower.")
+
+# Play the game
+guessTheNumber()
